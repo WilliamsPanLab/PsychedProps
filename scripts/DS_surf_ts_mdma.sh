75e69b4dcec0a13a5b17f6982d8cc3e1f8ec489b
adpines <apines@stanford.edu>
2022-11-26 18:16:16 -0800

    mdma support

---
+++ scripts/DS_surf_ts_mdma.sh
@@ -0,0 +1,40 @@
+subj=$1
+sesh=$2
+
+# set freesurfer dir
+export FREESURFER_HOME=/share/software/user/open/freesurfer/6.0.0
+# set subjs dir
+export SUBJECTS_DIR=/share/software/user/open/freesurfer/6.0.0/subjects
+# set freesurfer license
+export FS_LICENSE=/oak/stanford/groups/leanew1/users/apines/license.txt
+# mdma fp
+parentfp=/scratch/groups/leanew1/xcpd_outMDMA/xcp_d/${subj}/${sesh}/func
+childfp=/scratch/users/apines/data/mdma/${subj}/${sesh}
+
+# make output dirs
+mkdir ${childfp} -p
+
+# concat phase encoding directions
+wb_command -cifti-merge ${childfp}/${subj}_${sesh}_rs_concat.dtseries.nii -cifti ${parentfp}/${subj}_${sesh}_task-rs_acq-mb_dir-pe0_run-0_space-fsLR_den-91k_desc-denoisedSmoothed_bold.dtseries.nii -cifti ${parentfp}/${subj}_${sesh}_task-rs_acq-mb_dir-pe1_run-0_space-fsLR_den-91k_desc-denoisedSmoothed_bold.dtseries.nii
+
+# subject's aggregated time series
+AgTS=${childfp}/${subj}_${sesh}_rs_concat.dtseries.nii
+
+# separate hemispheres - left
+wb_command -cifti-separate $AgTS COLUMN -metric CORTEX_LEFT ${childfp}/${subj}_${sesh}_L_AggTS.func.gii 
+
+# right hemi
+wb_command -cifti-separate $AgTS COLUMN -metric CORTEX_RIGHT ${childfp}/${subj}_${sesh}_R_AggTS.func.gii
+
+### resample both hemis to 2.5k vertices
+# left hemisphere
+wb_command -metric-resample ${childfp}/${subj}_${sesh}_L_AggTS.func.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fs_LR-deformed_to-fsaverage.L.sphere.32k_fs_LR.surf.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fsaverage4_std_sphere.L.3k_fsavg_L.surf.gii ADAP_BARY_AREA ${childfp}/${subj}_${sesh}_L_AggTS_3k.func.gii -area-metrics /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fs_LR.L.midthickness_va_avg.32k_fs_LR.shape.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fsaverage4.L.midthickness_va_avg.3k_fsavg_L.shape.gii
+
+# right hemisphere
+wb_command -metric-resample ${childfp}/${subj}_${sesh}_R_AggTS.func.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fs_LR-deformed_to-fsaverage.R.sphere.32k_fs_LR.surf.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fsaverage4_std_sphere.L.3k_fsavg_L.surf.gii ADAP_BARY_AREA ${childfp}/${subj}_${sesh}_R_AggTS_3k.func.gii -area-metrics /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fs_LR.R.midthickness_va_avg.32k_fs_LR.shape.gii /oak/stanford/groups/leanew1/users/apines/standard_mesh_atlases/resample_fsaverage/fsaverage4.R.midthickness_va_avg.3k_fsavg_R.shape.gii
+
+# convert to mgh for reading individual hemisphere time series into matlab
+/share/software/user/open/freesurfer/6.0.0/bin/mri_convert.bin ${childfp}/${subj}_${sesh}_L_AggTS_3k.func.gii ${childfp}/${subj}_${sesh}_L_AggTS_3k.mgh
+/share/software/user/open/freesurfer/6.0.0/bin/mri_convert.bin ${childfp}/${subj}_${sesh}_R_AggTS_3k.func.gii ${childfp}/${subj}_${sesh}_R_AggTS_3k.mgh
+
+
---
+++ scripts/sbatch_OpFlow_MDMA.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+#SBATCH --job-name=OpFl
+#SBATCH --time=5:30:00
+#SBATCH -n 1
+#SBATCH --mem=30G
+#SBATCH -p leanew1,normal  # Queue names you can submit to
+# Outputs ----------------------------------
+#SBATCH --mail-user=no@stanford.edu
+#SBATCH --mail-type=ALL
+# ------------------------------------------
+
+# will need matlab
+module load matlab
+# and freesurfer
+module load biology
+module load freesurfer/7.3.2
+# and workbench
+module load workbench
+
+# subject name is input argument
+subj=$1
+# sesh is input 2
+sesh=$2
+
+# Downsample the data 
+/oak/stanford/groups/leanew1/users/apines/scripts/OpFl_CDys/scripts/DS_surf_ts_mdma.sh $1 $2
+
+# cd to scripts directory
+cd /oak/stanford/groups/leanew1/users/apines/scripts/OpFl_CDys/scripts
+
+# Calculate Optical Flow
+matlab -nodisplay -r "OpFl_mdma('$subj','$sesh')"
+
+# RS filepaths
+childfp=/scratch/users/apines/data/mdma/${subj}/${sesh}
+rsIn=${childfp}/${subj}_${sesh}_OpFl_rs.mat
+rsOut=${childfp}/${subj}_${sesh}_PGGDist_rs.mat
+
+# Calculate Angular Distance: rs
+/oak/stanford/groups/leanew1/users/apines/scripts/OpFl_CDys/scripts/run_PGG_AngDistCalc4_CompVer_PL.sh /share/software/user/restricted/matlab/R2018a/ $rsIn $rsOut
+
+# make output directory outside scratch
+mkdir /oak/stanford/groups/leanew1/users/apines/OpFlAngDs/mdma/${subj} 
+
+./run_Extract_BUTD_ResultantVecs_PL.sh /share/software/user/restricted/matlab/R2018a/ $rsIn $rsOut /oak/stanford/groups/leanew1/users/apines/OpFlAngDs/mdma/${subj}/${subj}_${sesh}_rs_BUTD_L.mat /oak/stanford/groups/leanew1/users/apines/OpFlAngDs/mdma/${subj}/${subj}_${sesh}_rs_BUTD_R.mat
+
+# convert to R format
+matlab -nodisplay -r "BUTD_to_Rformat_MDMA('$subj','$sesh')"
+
