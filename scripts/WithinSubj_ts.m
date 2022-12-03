70c162d8a420bfde54f35995101d8ee17e8fe059
adpines <apines@stanford.edu>
2022-12-02 18:27:50 -0800

    bug fix

--- scripts/WithinSubj_ts.m
+++ scripts/WithinSubj_ts.m
@@ -45,6 +45,8 @@ fmwIndVec_r=find(F_MW_R);
 % make medial wall vector
 g_noMW_combined_L=setdiff([1:5120],fmwIndVec_l);
 g_noMW_combined_R=setdiff([1:5120],fmwIndVec_r);
+noMW_L=setdiff(1:2562,mwIndVec_l);
+noMW_R=setdiff(1:2562,mwIndVec_r);
 
 % label filepath common for each subj
 commonFP=['/scratch/users/apines/data/mdma/'];
@@ -58,47 +60,68 @@ subjList=strcat(subjPrefix,subjSuffix')
 for s=[1 2 3 5 6 7 8 9 10 11 12 13 14 15 16 17]
 	% get session info
 	seshInfo=subSeshDose{s,2:5};
-	bvFP=[commonFP subjList(s) '/' seshInfo{1} '/' subjList(s) '_' seshInfo{1} '_PGGDist_rs.mat'];
+	bvFP=[commonFP subjList(s) '/' seshInfo{1} '/OpFl_timeseries_L.mat'];
 	bvFP=strjoin(bvFP,'')
-	pFP=[commonFP subjList(s) '/' seshInfo{2} '/' subjList(s) '_' seshInfo{2} '_PGGDist_rs.mat'];
+	bvFPr=[commonFP subjList(s) '/' seshInfo{1} '/OpFl_timeseries_R.mat'];
+        bvFPr=strjoin(bvFPr,'')
+	pFP=[commonFP subjList(s) '/' seshInfo{2} '/OpFl_timeseries_L.mat'];
 	pFP=strjoin(pFP,'')
-	m1FP=[commonFP subjList(s) '/' seshInfo{3} '/' subjList(s) '_' seshInfo{3} '_PGGDist_rs.mat'];
+	pFPr=[commonFP subjList(s) '/' seshInfo{2} '/OpFl_timeseries_R.mat'];
+        pFPr=strjoin(pFPr,'')
+	m1FP=[commonFP subjList(s) '/' seshInfo{3} '/OpFl_timeseries_L.mat'];
 	m1FP=strjoin(m1FP,'')
-	m2FP=[commonFP subjList(s) '/' seshInfo{4} '/' subjList(s) '_' seshInfo{4} '_PGGDist_rs.mat'];
+	m1FPr=[commonFP subjList(s) '/' seshInfo{3} '/OpFl_timeseries_R.mat'];
+        m1FPr=strjoin(m1FPr,'')
+	m2FP=[commonFP subjList(s) '/' seshInfo{4} '/OpFl_timeseries_L.mat'];
 	m2FP=strjoin(m2FP,'')
+	m2FPr=[commonFP subjList(s) '/' seshInfo{4} '/OpFl_timeseries_R.mat'];
+        m2FPr=strjoin(m2FPr,'')
 	% load in baseline session
 	bv=load(bvFP);
+	% pull out time series of interest
+	bv=squeeze(bv.LvertTS(:,2,:));
+	bvr=load(bvFPr);
+	bvr=squeeze(bvr.RvertTS(:,2,:));
 	% load in placebo session
 	p1=load(pFP);
+	p1=squeeze(p1.LvertTS(:,2,:));
+	p1r=load(pFPr);
+	p1r=squeeze(p1r.RvertTS(:,2,:));
 	% mdma sessions
 	m1=load(m1FP);
+	m1=squeeze(m1.LvertTS(:,2,:));
+	m1r=load(m1FPr);
+	m1r=squeeze(m1r.RvertTS(:,2,:));
 	m2=load(m2FP);
+	m2=squeeze(m2.LvertTS(:,2,:));
+	m2r=load(m2FPr);
+	m2r=squeeze(m2r.RvertTS(:,2,:));
 	% concat sober into one ts
-	sobLeft=vertcat(bv.AngDist.gLeft,p1.AngDist.gLeft);
-	sobRight=vertcat(bv.AngDist.gRight,p1.AngDist.gRight);
+	sobLeft=horzcat(bv,p1);
+	sobRight=horzcat(bvr,p1r);
 	% concat mdma into one ts
-	mLeft=vertcat(m1.AngDist.gLeft,m2.AngDist.gLeft);
-	mRight=vertcat(m1.AngDist.gRight,m2.AngDist.gRight);
+	mLeft=horzcat(m1,m2);
+	mRight=horzcat(m1r,m2r);
 	% init tvec and pvec
-	tvec_L=zeros(length(g_noMW_combined_L),1);
-	pvec_L=zeros(length(g_noMW_combined_L),1);
-	tvec_R=zeros(length(g_noMW_combined_R),1);
-	pvec_R=zeros(length(g_noMW_combined_R),1);
+	tvec_L=zeros(length(noMW_L),1);
+	pvec_L=zeros(length(noMW_L),1);
+	tvec_R=zeros(length(noMW_R),1);
+	pvec_R=zeros(length(noMW_R),1);
 	counter=0;
-	% for each left face
-	for F=g_noMW_combined_L
+	% for each left vert
+	for F=noMW_L
 		counter=counter+1;
 		% t-test
-		[h,p,ci,stats] = ttest(sobLeft(:,F),mLeft(:,F));
+		[h,p,ci,stats] = ttest(cell2mat(sobLeft(F,:)),cell2mat(mLeft(F,:)));
 		tvec_L(counter)=stats.tstat;
 		pvec_L(counter)=p;
 	end
 	counter=0;
 	% for each right face
-	for F=g_noMW_combined_R
+	for F=noMW_R
 		counter=counter+1;
 		% t-test
-                [h,p,ci,stats] = ttest(sobRight(:,F),mRight(:,F));
+                [h,p,ci,stats] = ttest(cell2mat(sobRight(F,:)),cell2mat(mRight(F,:)));
                 tvec_R(counter)=stats.tstat;
                 pvec_R(counter)=p;
 	end
@@ -110,11 +133,11 @@ for s=[1 2 3 5 6 7 8 9 10 11 12 13 14 15 16 17]
 	fdred=mafdr(ps);
 	% use it to thresh T's
 	ts(fdred>0.05)=0;
-	ts_L=ts(1:length(g_noMW_combined_L));
-	ts_R=ts((length(g_noMW_combined_L)+1):length(ts));
+	ts_L=ts(1:length(noMW_L));
+	ts_R=ts((length(noMW_L)+1):length(ts));
 	% png out filename
 	pngFN=['~/' subjList(s) '_ts.png'];
 	% visfacevec of threshed T's
-	Vis_Facevec(ts_L,ts_R,strjoin(pngFN,''))
+	Vis_Vertvec(ts_L,ts_R,strjoin(pngFN,''))
 end
 
