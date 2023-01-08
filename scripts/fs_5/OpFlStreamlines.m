18b0223aae151da575297bfb791c8ae571f52d2c
adpines <apines@stanford.edu>
2023-01-08 15:35:21 -0800

    minor streamlines

--- scripts/fs_5/OpFlStreamlines.m
+++ scripts/fs_5/OpFlStreamlines.m
@@ -1,6 +1,5 @@
 function OpFlStreamlines(subj,sesh,filename)
 
-
 % add paths
 addpath(genpath('/oak/stanford/groups/leanew1/users/apines/libs/'))
 childfp=['/scratch/users/apines/data/mdma/' subj '/' sesh ];
@@ -26,7 +25,6 @@ vx_l(numV+1:end, :) = VecNormalize(vx_l(numV+1:end, :));
 numV=length(vx_r);
 vx_r(numV+1:end, :) = VecNormalize(vx_r(numV+1:end, :));
 
-
 % calculate normative directionality streamlines for this subject and session
 n = size(faces_l, 1);
 T = TriRep(faces_l, vx_l);
@@ -34,28 +32,41 @@ P = T.incenters./100;
 
 %%% E as left Hemi
 E=data.us.vf_left;
+% initialize vector field to be plotted
+plotVF=zeros(n,3);
+% loop over to extract from unfortunate cell structure
+for i=1:length(E);
+	plotVF=plotVF+E{i};
+end
+% start with mean of each x y and z component
+plotVF=plotVF./length(E);
 
-%%% "k" will become circular mean. Doing single frame for now
-k=1;
-FrameOfInt=E{k};
+% consider circular mean: might require cart2sphvec
 
 % Set seed points.
-[X, Y] = meshgrid(-1:0.05:1, -1:0.05:1);
+[X, Y] = meshgrid(-1:0.04:1, -1:0.04:1);
 idx = find(X.^2 + Y.^2 <= 1);
 S = [X(idx), Y(idx)];
 
 % Set parameters.
-nmax = max(sqrt(sum((FrameOfInt).^2, 2)));
+nmax = max(sqrt(sum((plotVF).^2, 2)));
 h = 0.1/nmax;
-maxit = 50;
+maxit = 70;
 lw = 1;
 
 % Streamlines for first component.
-v = FrameOfInt;
+v = plotVF;
 
 F = createFigure('summer', -1, 1, -1, 1);
+% read in curvature as data for background surf
+curv=read_curv('/share/software/user/open/freesurfer/6.0.0/subjects/fsaverage5/surf/lh.curv']);
+% scale cortical mantle
+vx_l=vx_l./100;
+% add cortical mantle %
+aplot = trisurf(faces_l, vx_l(:,1), vx_l(:,2), vx_l(:,3),curv)
 streamlines2(P, v, S, h, maxit, 'summer', lw);
 %adjustFigure;
+% one rotation for insula
 savefigure(F, fullfile(childfp, filename), '-png', '-r600');
 %%%%%%%%%%%%%%
 
