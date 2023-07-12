ae59aaa63e020c5c882b2ed719aeee95631bb537
adpines <apines@stanford.edu>
2023-07-12 13:47:20 -0700

    test opfl percy nets mdma

---
+++ scripts/Netgiis_2_mat.m
@@ -0,0 +1,19 @@
+function Netgiis_2_mat(subj)
+% convert downsampled network .giis to .mats
+addpath(genpath('/oak/stanford/groups/leanew1/users/apines/libs/'))
+% this participant's filepath
+funcgiiFolder = ['/oak/stanford/groups/leanew1/users/apines/data/SingleParcel_1by1/' subj '-concat.dtseries.nii/IndividualParcel_Final_sbj1_comp4_alphaS21_1_alphaL300_vxInfo1_ard0_eta0/'];
+netgiis_L=[funcgiiFolder '' subj '_L_AggNets_3k.func.gii'];
+netgiis_R=[funcgiiFolder '' subj '_R_AggNets_3k.func.gii'];
+% load in functional networks: Left
+Lnets=gifti(netgiis_L);
+% load in functional networks: Right
+Rnets=gifti(netgiis_R);
+% set to 4 networks
+Lnets=Lnets.cdata(:,1:4);
+Rnets=Rnets.cdata(:,1:4);
+% save out
+nets=struct;
+nets.Lnets=Lnets;
+nets.Rnets=Rnets;
+save([funcgiiFolder '' subj '_Nets_fs4.mat'],'nets')
--- scripts/sbatch_OpFlow_MDMA.sh
+++ scripts/sbatch_OpFlow_MDMA.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=5:00:00
 #SBATCH -n 1
 #SBATCH --mem=85G
-#SBATCH -p leanew1,normal  # Queue names you can submit to
+#SBATCH -p leanew1 # Queue names you can submit to
 # Outputs ----------------------------------
 #SBATCH --mail-user=no@stanford.edu
 #SBATCH --mail-type=ALL
@@ -35,17 +35,28 @@ matlab -nodisplay -r "OpFl_mdma('$subj','$sesh')"
 # RS filepaths
 childfp=/scratch/users/apines/data/mdma/${subj}/${sesh}
 rsIn=${childfp}/${subj}_${sesh}_OpFl_rs.mat
-rsOut=${childfp}/${subj}_${sesh}_PGGDist_rs.mat
+rsOut=${childfp}/${subj}_${sesh}_NetDist_rs.mat
+
+# Convert networks to dscalar
+matlab -nodisplay -r "mat_to_dscalar('$subj')"
+
+# Downsample networks
+/oak/stanford/groups/leanew1/users/apines/scripts/FunctionalImages/Networks/DS_surf_Networks.sh $1
+
+# Convert them back to mats for ang distance calc (in matlab)
+matlab -nodisplay -r "Netgiis_2_mat('$subj')"
 
 # Calculate Angular Distance: rs
-/oak/stanford/groups/leanew1/users/apines/scripts/OpFl_CDys/scripts/run_PGG_AngDistCalc4_CompVer_PL.sh /share/software/user/restricted/matlab/R2018a/ $rsIn $rsOut
+#/oak/stanford/groups/leanew1/users/apines/scripts/OpFl_CDys/scripts/run_PGG_AngDistCalc4_CompVer_PL.sh /share/software/user/restricted/matlab/R2018a/ $rsIn $rsOut
 
 # make output directory outside scratch
- mkdir /oak/stanford/groups/leanew1/users/apines/OpFlAngDs/mdma/${subj} 
+mkdir /oak/stanford/groups/leanew1/users/apines/OpFlAngDs/mdma/${subj} 
+
+matlab -nodisplay -r "Extract_RelativeAngles('$subj','$rsIn')"
 
 #./run_Extract_BUTD_ResultantVecs_PL.sh /share/software/user/restricted/matlab/R2018a/ $rsIn $rsOut /oak/stanford/groups/leanew1/users/apines/OpFlAngDs/mdma/${subj}/${subj}_${sesh}_rs_BUTD_L.mat /oak/stanford/groups/leanew1/users/apines/OpFlAngDs/mdma/${subj}/${subj}_${sesh}_rs_BUTD_R.mat
 
 # convert to R format
-matlab -nodisplay -r "BUTD_to_Rformat_MDMA('$subj','$sesh')"
+#matlab -nodisplay -r "BUTD_to_Rformat_MDMA('$subj','$sesh')"
 
-./run_Extract_BUTD_ResultantVecs_Gran.sh /share/software/user/restricted/matlab/R2018a/ $rsIn $childfp/OpFl_timeseries_L.mat $childfp/OpFl_timeseries_R.mat
+#./run_Extract_BUTD_ResultantVecs_Gran.sh /share/software/user/restricted/matlab/R2018a/ $rsIn $childfp/OpFl_timeseries_L.mat $childfp/OpFl_timeseries_R.mat
