6fe76cfb8858a164d2b2a94e2c9c4b71f715a652
adpines <apines@stanford.edu>
2023-08-29 10:58:54 -0700

    interpolate to faces (masked)

--- scripts/InterpolateTS.m
+++ scripts/InterpolateTS.m
@@ -1,5 +1,6 @@
 function InterpolateTS(subj,sesh)
 % this function is to interpolate the functional time series to within-segment and between-TR timepoints. Should be v stable in band-passed fMR signal
+addpath(genpath('/oak/stanford/groups/leanew1/users/apines/libs/'))
 % load in time series (downsampled)
 childfp=['/scratch/users/apines/data/mdma/' subj '/' sesh ];
 % load in data
@@ -55,3 +56,72 @@ dataL.vol=interpolatedData_L;
 dataR.vol=interpolatedData_R;
 MRIwrite(dataL,ofpl)
 MRIwrite(dataR,ofpr)
+% now convert vertices to faces: will have to save out as .csv outside of .mgh framework
+SubjectsFolder = '/oak/stanford/groups/leanew1/users/apines/fs5surf';
+% for surface data
+surfL = [SubjectsFolder '/lh.sphere'];
+surfR = [SubjectsFolder '/rh.sphere'];
+% surface topography
+[vx_l, faces_l] = read_surf(surfL);
+[vx_r, faces_r] = read_surf(surfR);
+% +1 the faces: begins indexing at 0
+faces_l = faces_l + 1;
+faces_r = faces_r + 1;
+% faces_L
+F_L=faces_l;
+F_R=faces_r;
+% vertices V
+V_L=vx_l;
+% vertices V
+V_R=vx_r;
+% get incenters of triangles
+TR_L = TriRep(F_L,V_L);
+P_L = TR_L.incenters;
+TR_R = TriRep(F_R,V_R);
+P_R = TR_R.incenters;
+% initialize output face matrix
+tsLength=size(interpolatedData_L);
+tsLength=tsLength(2);
+faceOutL=zeros(20480,tsLength);
+faceOutR=zeros(20480,tsLength);
+% convert to faces
+for t=1:tsLength
+	interpolatedData_L_frame=interpolatedData_L(:,t);
+	faceOutL(:,t)=sum(interpolatedData_L_frame(faces_l),2)./3;
+	% right hemisphere
+	interpolatedData_R_frame=interpolatedData_R(:,t);
+	faceOutR(:,t)=sum(interpolatedData_R_frame(faces_r),2)./3;
+end
+% medial wall mask
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% use native freesurfer command for mw mask indices
+surfML = '/oak/stanford/groups/leanew1/users/apines/fs5surf/lh.Medial_wall.label';
+mwIndVec_l = read_medial_wall_label(surfML);
+surfMR = '/oak/stanford/groups/leanew1/users/apines/fs5surf/rh.Medial_wall.label';
+mwIndVec_r = read_medial_wall_label(surfMR);
+% make binary "is medial wall" vector for vertices
+mw_L=zeros(1,10242);
+mw_L(mwIndVec_l)=1;
+mw_R=zeros(1,10242);
+mw_R(mwIndVec_r)=1;
+% convert to faces
+F_MW_L=sum(mw_L(faces_l),2)./3;
+F_MW_R=sum(mw_R(faces_r),2)./3;
+% convert "partial" medial wall to medial wall
+F_MW_L=ceil(F_MW_L);
+F_MW_R=ceil(F_MW_R);
+% face mask indices
+fmwIndVec_l=find(F_MW_L);
+fmwIndVec_r=find(F_MW_R);
+% make medial wall vector
+g_noMW_combined_L=setdiff([1:20480],fmwIndVec_l);
+g_noMW_combined_R=setdiff([1:20480],fmwIndVec_r);
+%%%%%% mask it
+faceOutL=faceOutL(g_noMW_combined_L,:);
+faceOutR=faceOutR(g_noMW_combined_R,:);
+% saveout
+ofpl=[childfp '/' subj '_' sesh '_task-rs_p2mm_masked_interp_L_faces.csv'];
+ofpr=[childfp '/' subj '_' sesh '_task-rs_p2mm_masked_interp_R_faces.csv'];
+% saveout
+csvwrite(ofpl,faceOutL)
+csvwrite(ofpr,faceOutR)
