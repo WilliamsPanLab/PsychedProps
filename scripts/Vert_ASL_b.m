67c7e291fc6688874bfc53b343b7bd29007902b3
adpines <apines@stanford.edu>
2023-10-14 14:07:11 -0700

    below expected viz

--- scripts/Vert_ASL_b.m
+++ scripts/Vert_ASL_b.m
@@ -1,4 +1,4 @@
-function Vert_ASL(subj,sesh)
+function Vert_ASL_b(subj,sesh)
 % compute the vertex-wise anterior/superior/left vector from SigStreams
 addpath(genpath('/oak/stanford/groups/leanew1/users/apines/libs/'))
 % load in sigStreams
@@ -12,8 +12,8 @@ surfML = [SubjectsFolder '/lh.Medial_wall.label'];
 mwIndVec_l = read_medial_wall_label(surfML);
 % make "isn't medial wall" indices
 nonMW_L=setdiff(1:2562,mwIndVec_l);
-% initialize vector score matrix (2562 x 3 or subset of 2562, x 3)
-vectorScores=zeros(length(nonMW_L),3);
+% initialize vector location matrix (2562 x 3 or subset of 2562, x 3)
+vectorLoc=zeros(length(nonMW_L),3);
 % for each vertex
 for v=1:length(vx_l(nonMW_L))
 	% get euclidean location of this vertex (x,y,z)
@@ -31,20 +31,17 @@ for v=1:length(vx_l(nonMW_L))
 		currcell=sigStreamRow(v2);
 		% get euclidean location of cell
 		eucLocV2=vx_l(nonMW_L(v2),:);
-		% get vector from v to v2
-		vectorV2V=eucLocV2-eucLocV;
-		% multiply by counts, can later ammend to sqrt of counts
-		vectorV2V=vectorV2V*currcell;
-		% add derived vector to vector score
-		vectorScore=vectorScore+vectorV2V;
+		% plug into coordinate set
+		coordSet(find(nonzvs==v2),:)=eucLocV2;
 	end
+	% get mean location
+	meanLoc=mean(coordSet);
 	% populate vector score matrix
-	vectorScores(v,:)=vectorScore;
+	vectorLoc(v,:)=meanLoc;
 end
 % make a medial wall version
-MWvectorscores=zeros(2562,3);
-MWvectorscores(nonMW_L,:)=vectorScores;
+MWvectorlocations=zeros(2562,3);
+MWvectorlocations(nonMW_L,:)=vectorLoc;
 
 % saveout vector score matrix
-save(['/scratch/users/apines/SimStreams/' subj '_' sesh '_vectorScores.mat'],'MWvectorscores')
-end
+save(['/scratch/users/apines/SimStreams/' subj '_' sesh '_vectorLocs.mat'],'MWvectorlocations')
