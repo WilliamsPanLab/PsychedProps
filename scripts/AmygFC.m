e6e43aa4477f6312ed80b655cd007dfe84722d0d
adpines <apines@stanford.edu>
2023-11-01 16:36:38 -0700

    working fc agg

--- scripts/AmygFC.m
+++ scripts/AmygFC.m
@@ -1,15 +1,14 @@
-function AmygFC(subj,sesh)
-% establish normative FC between Nac Shell,Lat Amyg, MD thal, and DMN in this dataset
+function AmygFC
+%establish normative FC between Nac Shell,Lat Amyg, MD thal, and DMN in this dataset
 scale3fp='/oak/stanford/groups/leanew1/users/apines/maps/Tian_Subcortex_S3_3T_32k.dlabel.nii';
 % load in concatenated cifti, calculate FC for this subject, print out onto two maps (L and R)
-Paths{1} = '/oak/stanford/groups/leanew1/users/apines/scripts/PersonalCircuits/scripts/code_nmf_cifti/tool_folder'; 
+Paths{1} = '/oak/stanford/groups/leanew1/users/apines/scripts/PersonalCircuits/scripts/code_nmf_cifti/tool_folder';
 addpath(genpath(Paths{1}))
-% set cifti path
-ciftipath = ['/scratch/groups/leanew1/xcpd_outP50_36p_bp/xcp_d/' subj  '/ses-00/func/concatenated.dtseries.nii'];
-% load in cifti
-concatData=read_cifti(ciftipath);
-% load in scale 3 data
+
+% load in atlas
 scale_3=read_cifti(scale3fp);
+
+% load in scale 3 data
 % 9 for right DM thal
 DMT_rInd=find(scale_3.cdata==9);
 % 34 for left DM thal
@@ -40,8 +39,8 @@ R32k(scale_3.diminfo{1}.models{2}.vertlist+1)=1;
 L59k_dmn=dmnL(logical(L32k));
 R59k_dmn=dmnR(logical(R32k));
 % get dmn indices
-dmnIndL=find(L59k_dmn>.3);
-dmnIndR=find(R59k_dmn>.3);
+dmnIndL=find(L59k_dmn>.4);
+dmnIndR=find(R59k_dmn>.4);
 % add starting value to DMNindR
 dmnIndR=dmnIndR+scale_3.diminfo{1}.models{2}.start-1;
 
@@ -51,7 +50,7 @@ templateCifti.cdata=zeros(91282,1);
 templateCifti.cdata(dmnIndL)=1;
 templateCifti.cdata(dmnIndR)=1;
 templateCifti.cdata(DMT_lInd)=2;
-templateCifti.cdata(DMT_rInd)=2;
+templateCifti.cdata(DMT_rInd)2;
 templateCifti.cdata(NAC_lInd)=3;
 templateCifti.cdata(NAC_rInd)=3;
 templateCifti.cdata(AmyLat_lInd)=4;
@@ -59,99 +58,126 @@ templateCifti.cdata(AmyLat_rInd)=4;
 write_cifti(templateCifti,'/oak/stanford/groups/leanew1/users/apines/maps/testAtlas.dscalar.nii')
 % test checks out
 
-% extract DM thalamic TS - left and right
-% left
-DMT_ts_L=concatData.cdata(DMT_lInd,:);
-DMT_mts_L=mean(DMT_ts_L);
-% right
-DMT_ts_R=concatData.cdata(DMT_rInd,:);
-DMT_mts_R=mean(DMT_ts_R);
+% loop over each subject
+% read in subject dosage correspondence, has to be before addpath for some silly reason
+subSeshDose=readtable('~/subjSeshDoseCorresp.csv');
+tasks = {'rs1', 'rs2','gambling','emotion','wm','conscious','gonogo','nonconscious'};
+% combine angular distance from reference streams across subjects
+subjects = {'sub-MDMA001', 'sub-MDMA002', 'sub-MDMA003', 'sub-MDMA005', 'sub-MDMA007', 'sub-MDMA008', 'sub-MDMA009', 'sub-MDMA011', 'sub-MDMA012', 'sub-MDMA013', 'sub-MDMA014', 'sub-MDMA015', 'sub-MDMA016', 'sub-MDMA017'};
 
-% extract nac shell TS - left and right
-% left
-NAC_ts_L=concatData.cdata(NAC_lInd,:);
-NAC_mts_L=mean(NAC_ts_L);
-% right
-NAC_ts_R=concatData.cdata(NAC_rInd,:);
-NAC_mts_R=mean(NAC_ts_R);
+subjPrefix=repmat('sub-MDMA0',17,1);
+subjSuffix=["01","02","03","04","05","06","07","08","09","10","11","12","13","14","15","16","17"];
+subjList=strcat(subjPrefix,subjSuffix')
+% start iterator
+iterator=0
+corrMats=zeros(8,8,length([1 2 3 5 7 9 11 12 13 14 15 16 17]));
 
-% extract amygdalar TS - left and right
-% left
-A_ts_L=concatData.cdata(AmyLat_lInd,:);
-A_mts_L=mean(A_ts_L);
-% right
-A_ts_R=concatData.cdata(AmyLat_rInd,:);
-A_mts_R=mean(A_ts_R);
+% aggregate each subject, session, and task for each streams
+for s=[1 2 3 5 7 9 11 12 13 14 15 16 17]
+        % add 1 to iterator
+        iterator=iterator+1
+        % get subject name
+        subj=subjList{s}
+	% get session info
+        seshInfo=subSeshDose{s,2:5};
+        % for placebo, 80mg, and 120mg
+        seshArray={seshInfo{2} seshInfo{3} seshInfo{4}};i
+	% make an iterator for session
+        seshIterator=0;
+	for sessioncell=seshArray
+		sesh=sessioncell{1}
+		% update session iterator
+		seshIterator=seshIterator+1;seshIterator=seshIterator+1;		
+		% set cifti path
+		subjdir = ['/scratch/groups/leanew1/xcpd_outP50_36p_bp/xcp_d/' subj  '/' sesh  '/func/'];
+		% get list of files
+		subjfiles=dir(fullfile(subjdir));
+		% for each task
+		for taskcell=tasks
+			task=taskcell{1};
+			% get proper dtseries
+			matchingFiles = {};
+			for i = 1:length(subjfiles)
+           			if contains(subjfiles(i).name, task) && contains(subjfiles(i).name, 'desc-denoisedSmoothed_bold.dtseries.nii')
+                			matchingFiles = [matchingFiles, subjfiles(i).name];
+            			end
+        		end	
+			% reconstruct cifti path
+			ciftipath=[subjdir matchingFiles{1}];	
+			% load in cifti
+			concatData=read_cifti(ciftipath);
+			% extract DM thalamic TS - left and right
+			% left
+			DMT_ts_L=concatData.cdata(DMT_lInd,:);
+			DMT_mts_L=mean(DMT_ts_L);
+			% right
+			DMT_ts_R=concatData.cdata(DMT_rInd,:);
+			DMT_mts_R=mean(DMT_ts_R);
 
-% DMN TS - left and right
-% left
-DMN_ts_L=concatData.cdata(dmnIndL,:);
-DMN_mts_L=mean(DMN_ts_L);
-% right
-DMN_ts_R=concatData.cdata(dmnIndR,:);
-DMN_mts_R=mean(DMN_ts_R);
+			% extract nac shell TS - left and right
+			% left
+			NAC_ts_L=concatData.cdata(NAC_lInd,:);
+			NAC_mts_L=mean(NAC_ts_L);
+			% right
+			NAC_ts_R=concatData.cdata(NAC_rInd,:);
+			NAC_mts_R=mean(NAC_ts_R);
 
-% get big correlation matrix between all derived mean-time series
-% initialize matrix
-corrMat=zeros(8,8);
-% fill in
-corrMat(1,2)=corr(DMT_mts_L',DMT_mts_R');
-corrMat(1,3)=corr(DMT_mts_L',NAC_mts_L');
-corrMat(1,4)=corr(DMT_mts_L',NAC_mts_R');
-corrMat(1,5)=corr(DMT_mts_L',A_mts_L');
-corrMat(1,6)=corr(DMT_mts_L',A_mts_R');
-corrMat(1,7)=corr(DMT_mts_L',DMN_mts_L');
-corrMat(1,8)=corr(DMT_mts_L',DMN_mts_R');
-corrMat(2,3)=corr(DMT_mts_R',NAC_mts_L');
-corrMat(2,4)=corr(DMT_mts_R',NAC_mts_R');
-corrMat(2,5)=corr(DMT_mts_R',A_mts_L');
-corrMat(2,6)=corr(DMT_mts_R',A_mts_R');
-corrMat(2,7)=corr(DMT_mts_R',DMN_mts_L');
-corrMat(2,8)=corr(DMT_mts_R',DMN_mts_R');
-corrMat(3,4)=corr(NAC_mts_L',NAC_mts_R');
-corrMat(3,5)=corr(NAC_mts_L',A_mts_L');
-corrMat(3,6)=corr(NAC_mts_L',A_mts_R');
-corrMat(3,7)=corr(NAC_mts_L',DMN_mts_L');
-corrMat(3,8)=corr(NAC_mts_L',DMN_mts_R');
-corrMat(4,5)=corr(NAC_mts_R',A_mts_L');
-corrMat(4,6)=corr(NAC_mts_R',A_mts_R');
-corrMat(4,7)=corr(NAC_mts_R',DMN_mts_L');
-corrMat(4,8)=corr(NAC_mts_R',DMN_mts_R');
-corrMat(5,6)=corr(A_mts_L',A_mts_R');
-corrMat(5,7)=corr(A_mts_L',DMN_mts_L');
-corrMat(5,8)=corr(A_mts_L',DMN_mts_R');
-corrMat(6,7)=corr(A_mts_R',DMN_mts_L');
-corrMat(6,8)=corr(A_mts_R',DMN_mts_R');
-corrMat(7,8)=corr(DMN_mts_L',DMN_mts_R');
-% fill in lower triangle
-corrMat=corrMat+corrMat'-diag(diag(corrMat));
-
-
-
-
-
-
-% get num of grayords
-numGrayords=size(concatData.cdata);
-numGrayords=numGrayords(1);
-% initialize L amyg FC vec
-LamygFC=zeros(1,numGrayords);
-% R
-RamygFC=zeros(1,numGrayords);
-
-
-% get correlations across rest of brain (grayordinate-wise)
-for g=1:numGrayords
-	LamygFC(g)=corr(concatData.cdata(g,:)',A_mts_L');
-	RamygFC(g)=corr(concatData.cdata(g,:)',A_mts_R');
-	PCCFC(g)=corr(concatData.cdata(g,:)',PCC_ts');
+			% extract amygdalar TS - left and right
+			% left
+			A_ts_L=concatData.cdata(AmyLat_lInd,:);
+			A_mts_L=mean(A_ts_L);
+			% right
+			A_ts_R=concatData.cdata(AmyLat_rInd,:);
+			A_mts_R=mean(A_ts_R);
+	
+			% DMN TS - left and right
+			% left
+			DMN_ts_L=concatData.cdata(dmnIndL,:);
+			DMN_mts_L=mean(DMN_ts_L);
+			% right
+			DMN_ts_R=concatData.cdata(dmnIndR,:);
+			DMN_mts_R=mean(DMN_ts_R);
+	
+			% get big correlation matrix between all derived mean-time series
+			% initialize matrix
+			corrMat=zeros(8,8);
+			% fill in
+			corrMat(1,2)=corr(DMT_mts_L',DMT_mts_R');
+			corrMat(1,3)=corr(DMT_mts_L',NAC_mts_L');
+			corrMat(1,4)=corr(DMT_mts_L',NAC_mts_R');
+			corrMat(1,5)=corr(DMT_mts_L',A_mts_L');
+			corrMat(1,6)=corr(DMT_mts_L',A_mts_R');
+			corrMat(1,7)=corr(DMT_mts_L',DMN_mts_L');
+			corrMat(1,8)=corr(DMT_mts_L',DMN_mts_R');
+			corrMat(2,3)=corr(DMT_mts_R',NAC_mts_L');
+			corrMat(2,4)=corr(DMT_mts_R',NAC_mts_R');
+			corrMat(2,5)=corr(DMT_mts_R',A_mts_L');
+			corrMat(2,6)=corr(DMT_mts_R',A_mts_R');
+			corrMat(2,7)=corr(DMT_mts_R',DMN_mts_L');
+			corrMat(2,8)=corr(DMT_mts_R',DMN_mts_R');
+			corrMat(3,4)=corr(NAC_mts_L',NAC_mts_R');
+			corrMat(3,5)=corr(NAC_mts_L',A_mts_L');
+			corrMat(3,6)=corr(NAC_mts_L',A_mts_R');
+			corrMat(3,7)=corr(NAC_mts_L',DMN_mts_L');
+			corrMat(3,8)=corr(NAC_mts_L',DMN_mts_R');
+			corrMat(4,5)=corr(NAC_mts_R',A_mts_L');
+			corrMat(4,6)=corr(NAC_mts_R',A_mts_R');
+			corrMat(4,7)=corr(NAC_mts_R',DMN_mts_L');
+			corrMat(4,8)=corr(NAC_mts_R',DMN_mts_R');
+			corrMat(5,6)=corr(A_mts_L',A_mts_R');
+			corrMat(5,7)=corr(A_mts_L',DMN_mts_L');
+			corrMat(5,8)=corr(A_mts_L',DMN_mts_R');
+			corrMat(6,7)=corr(A_mts_R',DMN_mts_L');
+			corrMat(6,8)=corr(A_mts_R',DMN_mts_R');
+			corrMat(7,8)=corr(DMN_mts_L',DMN_mts_R');
+			% fill in lower triangle
+			corrMat=corrMat+corrMat'-diag(diag(corrMat));
+			% save correlation matrix
+		        % extract unitary AmFug FC
+			% populate unitary AmFug FC	
+		end
+	end
 end
-% load in template cifti
-tCifti = read_cifti('/oak/stanford/groups/leanew1/users/apines/maps/hcp.gradients.dscalar.nii')
-% replace
-tCifti.cdata(:,1)=LamygFC;
-tCifti.cdata(:,2)=RamygFC;
-tCifti.cdata(:,3)=PCCFC;
-% saveout into new cifti
-write_cifti(tCifti,['/scratch/groups/leanew1/xcpd_outP50_36p_bp/bv_concat/' subj  '_amygFC.dscalar.nii']);
-
+% save out a long-format subj, dose, task dataframe from uniary AmFug FC structure
+% get average correlation matrix
+meanCor=mean(corrMats,3);
