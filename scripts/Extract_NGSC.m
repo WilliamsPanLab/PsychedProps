b439845e028c918ecee6f829ef7430032c258efa
adpines <apines@stanford.edu>
2023-12-20 16:27:39 -0800

    update for complexity, note need to port scrubbing mask to TR-wise

--- scripts/Extract_NGSC.m
+++ scripts/Extract_NGSC.m
@@ -45,20 +45,22 @@ for p=DMNParcels
 	% apply to time series
 	dmn_ts=C_timeseries(logical(GP.data==p),:);
 	% conduct temporal PCA to recover components m (# of grayordinates)
-	[coeff, ~, explained] = pca(dmn_ts);
+	%[coeff, ~, explained] = pca(dmn_ts);
 	%[coeff,score,latent,tsquared,explained,mu] = pca(dmn_ts);
 	% get normalized eigenvalue of each principal component (divided by sum of eigenvalues)
 	% https://stackoverflow.com/questions/30792185/matlab-how-to-obtain-the-eigenvalues-from-the-pca
-	norm_eig=explained/sum(explained);
-
+	%norm_eig=explained/sum(explained);
 	% get the numerator for normalized entropy of the normalized eigenvalues, sum of the normalized eigenvalue * log(normalized eigenvalue) for each component
-	numerator = sum(norm_eig .* log(norm_eig));
-
+	%numerator = sum(norm_eig .* log(norm_eig));
 	% get the denominator for normalized entropy of the normalized eigenvalues, log of the number of components
-	denominator = log(length(norm_eig));
-
+	%denominator = log(length(norm_eig));
 	% get normalized entropy of the normalized eigenvalues, numerator divided by denominator * -1
-	nGSC = -1 * (numerator / denominator);
+	%nGSC = -1 * (numerator / denominator);
+	
+	% explicitly using Josh's code, note scrubbing mask needed
+	[~,~,~,~,EXPLAINED]=pca(dmn_ts);
+	EXPLAINED=EXPLAINED/100;
+	nGSC=-sum(EXPLAINED .* log(EXPLAINED))/log(length(EXPLAINED));
 	cxDMN = [cxDMN nGSC];
 end
 % can insert parcelwise saveout here later if interested
