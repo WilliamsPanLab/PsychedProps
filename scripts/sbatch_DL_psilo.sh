7b78edef371a25432140cb4b5114aaa5399ba0a2
adpines <apines@stanford.edu>
2024-01-20 13:22:45 -0800

    DL and smoothing

--- scripts/sbatch_DL_psilo.sh
+++ scripts/sbatch_DL_psilo.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=4:00:00
 #SBATCH -n 4
 #SBATCH --mem=30G
-#SBATCH -p leanew1  # Queue names you can submit to
+#SBATCH -p normal,leanew1  # Queue names you can submit to
 # Outputs ----------------------------------
 #SBATCH --mail-user=apines@stanford.edu
 #SBATCH --mail-type=ALL
