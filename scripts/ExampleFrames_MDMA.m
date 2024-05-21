14d7b601a2c0f0cf3ca3eba83c472494fdc43e78
adpines <apines@stanford.edu>
2024-05-20 17:16:27 -0700

    instance visualization working

--- scripts/ExampleFrames_MDMA.m
+++ scripts/ExampleFrames_MDMA.m
@@ -42,7 +42,8 @@ vfs_R=VFs.vf_right;
 lengthOF=length(VFs.vf_left);
 % get number of faces
 lengthFaces=length(VFs.vf_left{1});
-
+% get length of vertices
+lengthVertices=length(TRs_l);
 %%% load in freesurfer surfaces
 % Load in surface data - sphere
 surfL = ['/oak/stanford/groups/leanew1/users/apines/surf/lh.sphere'];
@@ -69,35 +70,6 @@ L_inc = TriR_L.incenters;
 TriR_R = TriRep(faces_ri, vx_ri);
 R_inc = TriR_R.incenters;
 
-% create colormap
-% roy-big-bl palette imitation, inferno is just template
-roybigbl_cm=inferno(16);
-roybigbl_cm(1,:)=[255, 255, 0 ];
-roybigbl_cm(2,:)=[255, 200, 0];
-roybigbl_cm(3,:)=[255, 120, 0];
-roybigbl_cm(4,:)=[255, 0, 0 ];
-roybigbl_cm(5,:)=[200, 0, 0 ];
-roybigbl_cm(6,:)=[150, 0, 0 ];
-roybigbl_cm(7,:)=[100, 0, 0 ];
-roybigbl_cm(8,:)=[60, 0, 0 ];
-roybigbl_cm(9,:)=[0, 0, 80 ];
-roybigbl_cm(10,:)=[0, 0, 170];
-roybigbl_cm(11,:)=[75, 0, 125];
-roybigbl_cm(12,:)=[125, 0, 160];
-roybigbl_cm(13,:)=[75, 125, 0];
-roybigbl_cm(14,:)=[0, 200, 0];
-roybigbl_cm(15,:)=[0, 255, 0];
-roybigbl_cm(16,:)=[0, 255, 255];
-% scale to 1
-roybigbl_cm=roybigbl_cm.*(1/255);
-% interpolate color gradient
-interpsteps=[0 0.0666 0.1333 0.2 0.2666 0.3333 0.4 0.4666 0.5333 0.6 0.6666 0.7333 0.8 0.86666 0.9333 1];
-roybigbl_cm=interp1(interpsteps,roybigbl_cm,linspace(0,1,255));
-% yellow as high
-roybigbl_cm=flipud(roybigbl_cm);
-% reduce just a little bit on the close-to-white coloring
-roybigbl_cm=roybigbl_cm(15:240,:);
-
 % for each prop
 for prop=1:3
 	% pull out this propagation timepoint
@@ -115,119 +87,103 @@ for prop=1:3
 		signalOfInt_L=TRs_l(:,fr);
 		signalOfInt_R=TRs_r(:,fr);
 		% initialzie flattened vectors array for this frame
-		flattenedVectors_L=zeros(lengthFaces,3);
-		flattenedVectors_R=zeros(lengthFaces,3);	
+		flattenedVectors_L=zeros(lengthVertices,3);
+		flattenedVectors_R=zeros(lengthVertices,3);	
 		% retrieve original vectors
                 OGVecs_L=vfs_L{fr};
                 OGVecs_R=vfs_R{fr};
 		% for each point in space
-		for F=1:lengthFaces
-			% retrieve original vector
-			OGVec_L=OGVecs_L(F,:);
-			OGVec_R=OGVecs_R(F,:);
-			% find each neighboring face: faces that share two vertices (should be 3)
-			currFace_L=faces_l(F,:);
-			currFace_R=faces_r(F,:);
-			% Initialize neighboring faces storage
-            		neighboringFaces_L = [];
-            		neighboringFaces_R = [];
-			% Find neighboring faces for the left hemisphere
-            		for f = 1:size(faces_l, 1)
-                		if f ~= F
-                		    commonVertices = intersect(currFace_L, faces_l(f, :));
-                		    if length(commonVertices) == 2
-                		        neighboringFaces_L = [neighboringFaces_L; f];
-		   		    end
-               		 	end
-            		end
-			% right hemi
-            		for f = 1:size(faces_r, 1)
-                                if f ~= F
-                                    commonVertices = intersect(currFace_R, faces_r(f, :));
-                                    if length(commonVertices) == 2
-                                        neighboringFaces_R = [neighboringFaces_R; f];   
-                                    end   
-                                end
-                        end
+		for vertInd=1:lengthVertices
+			% get faces involved in this vertex
+			[InvolvedFaces_l,~]=find(faces_l==vertInd);
+			[InvolvedFaces_r,~]=find(faces_r==vertInd);
+			% retrieve original vectors and average them across these faces
+			OGVec_L=mean(OGVecs_L(InvolvedFaces_l,:));
+			OGVec_R=mean(OGVecs_R(InvolvedFaces_r,:));
 			% initialize projection vector (directionality and magnitude w/r/t neighboring faces)
-			projection_L=zeros(3,3);	
+			projection_L=zeros(5,3);
+			projection_R=zeros(5,3);	
 			faceCounter=1;
-			% construe vector in terms of how much it points to each neighboring face
-            		for neighborFace = neighboringFaces_L'
-                		% Calculate normal vector for the neighboring face
+			% constriue vector in terms of how much it points to each neighboring face
+			for neighborFace = InvolvedFaces_l'
+               			% Calculate normal vector for the neighboring face
                 		normalVectors = cross(vx_l(faces_l(neighborFace, 2), :) - vx_l(faces_l(neighborFace, 1), :), ...
-                                      vx_l(faces_l(neighborFace, 3), :) - vx_l(faces_l(neighborFace, 1), :));
+                        		              vx_l(faces_l(neighborFace, 3), :) - vx_l(faces_l(neighborFace, 1), :));
                 		meanNormalVector = mean(normalVectors, 1);
                 		meanNormalVector = VecNormalize(meanNormalVector);
-                
-                		% Project the original vector onto the normal vector
-                		projection_L(faceCounter,:) = dot(OGVec_L, meanNormalVector) * meanNormalVector;
-				faceCounter=faceCounter+1;
-            		end
-			% right hemi
-			projection_R=zeros(3,3);
-                        faceCounter=1;
-                        % construe vector in terms of how much it points to each neighboring face
-                        for neighborFace = neighboringFaces_R'
-                                % Calculate normal vector for the neighboring face
-                                normalVectors = cross(vx_r(faces_r(neighborFace, 2), :) - vx_r(faces_r(neighborFace, 1), :), ...
-                                      vx_r(faces_r(neighborFace, 3), :) - vx_r(faces_r(neighborFace, 1), :));
-                                meanNormalVector = mean(normalVectors, 1);
-                                meanNormalVector = VecNormalize(meanNormalVector);
 
-                                % Project the original vector onto the normal vector
-                                projection_R(faceCounter,:) = dot(OGVec_R, meanNormalVector) * meanNormalVector;
-                                faceCounter=faceCounter+1;
-                        end
-			
-			% reconstruct on inflated surface in terms of how much it points to each neighboring face
-			reconstructedVec_L = zeros(1, 3);
-			for i = 1:length(neighboringFaces_L)
-                		neighborFace = neighboringFaces_L(i);
-                		newVertexCoords_L = vx_li(faces_li(neighborFace, :), :);
-                		meanNewVertex_L = mean(newVertexCoords_L, 1); % Get the mean position of the new face vertices
-                		reconstructedVec_L = reconstructedVec_L + projection_L(i, :) .* meanNewVertex_L;
+                		% Project the original vector onto the normal vector
+                		projection_L(faceCounter, :) = dot(OGVec_L, meanNormalVector) * meanNormalVector;
+                		faceCounter = faceCounter + 1;
             		end
-			% right hemisphere
-			reconstructedVec_R = zeros(1, 3);
-                        for i = 1:length(neighboringFaces_R)
-                                neighborFace = neighboringFaces_R(i);
-                                newVertexCoords_R = vx_ri(faces_ri(neighborFace, :), :);
-                                meanNewVertex_R = mean(newVertexCoords_R, 1); % Get the mean position of the new face vertices
-                                reconstructedVec_R = reconstructedVec_R + projection_R(i, :) .* meanNewVertex_R;
-                        end
+
+			% Construe vector in terms of how much it points to each neighboring face for right hemisphere
+            		faceCounter = 1;
+            		for neighborFace = InvolvedFaces_r'
+                		% Calculate normal vector for the neighboring face
+                		normalVectors = cross(vx_r(faces_r(neighborFace, 2), :) - vx_r(faces_r(neighborFace, 1), :), ...
+                        		              vx_r(faces_r(neighborFace, 3), :) - vx_r(faces_r(neighborFace, 1), :));
+                		meanNormalVector = mean(normalVectors, 1);
+                		meanNormalVector = VecNormalize(meanNormalVector);
+
+                		% Project the original vector onto the normal vector
+                		projection_R(faceCounter, :) = dot(OGVec_R, meanNormalVector) * meanNormalVector;
+                		faceCounter = faceCounter + 1;
+            		end
+			% Reconstruct the vector on the inflated surface (new surface)
+            		% Using vx_li and vx_ri for new surface vertices
+            		reconstructedVec_L = zeros(1, 3);
+            		for i = 1:length(InvolvedFaces_l)
+               		 	neighborFace = InvolvedFaces_l(i);
+                		newVertexCoords_L = vx_li(faces_li(neighborFace, :), :);
+            			meanNewVertex_L = mean(newVertexCoords_L, 1); % Get the mean position of the new face vertices
+            			reconstructedVec_L = reconstructedVec_L + projection_L(i, :) .* meanNewVertex_L; % Element-wise multiplication
+            		end
+            		% right hemisphere
+            		reconstructedVec_R = zeros(1, 3);
+            		for i = 1:length(InvolvedFaces_r)
+            		    neighborFace = InvolvedFaces_r(i);
+            		    newVertexCoords_R = vx_ri(faces_ri(neighborFace, :));
+            		    meanNewVertex_R = mean(newVertexCoords_R, 1); % Get the mean position of the new face vertices
+            		    reconstructedVec_R = reconstructedVec_R + projection_R(i, :) .* meanNewVertex_R; % Element-wise multiplication
+            		end
 			
+			% Normalize the reconstructed vectors
+            		reconstructedVec_L = VecNormalize(reconstructedVec_L);
+            		reconstructedVec_R = VecNormalize(reconstructedVec_R);	
+			
+
+			% Remove orthogonal component to flatten to the inflated surface
+            		% Left hemisphere
+            		involvedFaces_L = find(any(faces_li == vertInd, 2)); % Find faces involving this vertex
+            		normalVectors_L = cross(vx_li(faces_li(involvedFaces_L, 2), :) - vx_li(faces_li(involvedFaces_L, 1), :), ...
+                		                    vx_li(faces_li(involvedFaces_L, 3), :) - vx_li(faces_li(involvedFaces_L, 1), :));
+            		meanNormalVector_L = mean(normalVectors_L, 1);
+            		meanNormalVector_L = VecNormalize(meanNormalVector_L);
+            
+            		OGvecOrthogonal_L = dot(reconstructedVec_L, meanNormalVector_L) * meanNormalVector_L;
+            		modVec_L = reconstructedVec_L - OGvecOrthogonal_L;
+            		modVec_L = VecNormalize(modVec_L);
+
+            		% Right hemisphere
+            		involvedFaces_R = find(any(faces_ri == vertInd, 2)); % Find faces involving this vertex
+            		normalVectors_R = cross(vx_ri(faces_ri(involvedFaces_R, 2), :) - vx_ri(faces_ri(involvedFaces_R, 1), :), ...
+            		                        vx_ri(faces_ri(involvedFaces_R, 3), :) - vx_ri(faces_ri(involvedFaces_R, 1), :));
+            		meanNormalVector_R = mean(normalVectors_R, 1);
+            		meanNormalVector_R = VecNormalize(meanNormalVector_R);
+            
+            		OGvecOrthogonal_R = dot(reconstructedVec_R, meanNormalVector_R) * meanNormalVector_R;
+            		modVec_R = reconstructedVec_R - OGvecOrthogonal_R;
+            		modVec_R = VecNormalize(modVec_R);
+
+            		% Store the flattened vectors for later use or visualization
+            		flattenedVectors_L(vertInd, :) = modVec_L;
+            		flattenedVectors_R(vertInd, :) = modVec_R;
+
 			%	
 			%%%% Visualization check: compare spherical to inflated surface projection
 			%
 			%
-
-			% Remove orthogonal component to flatten to the inflated surface
-            		% Left hemisphere
-			involvedFace_L = F;
-            		normalVectors_L = cross(vx_li(faces_li(involvedFace_L, 2), :) - vx_li(faces_li(involvedFace_L, 1), :), ...
-            		                        vx_li(faces_li(involvedFace_L, 3), :) - vx_li(faces_li(involvedFace_L, 1), :));
-            		meanNormalVector_L = mean(normalVectors_L, 1);
-            		meanNormalVector_L = VecNormalize(meanNormalVector_L);
-            
-            		vecOrthogonal_L = dot(reconstructedVec_L, meanNormalVector_L) * meanNormalVector_L;
-            		modVec_L = reconstructedVec_L - vecOrthogonal_L;
-            		modVec_L = VecNormalize(modVec_L);
-
-            		% Right hemisphere
-			involvedFace_R = F;
-            		normalVectors_R = cross(vx_ri(faces_ri(involvedFace_R, 2), :) - vx_ri(faces_ri(involvedFace_R, 1), :), ...
-                		                    vx_ri(faces_ri(involvedFace_R, 3), :) - vx_ri(faces_ri(involvedFace_R, 1), :));
-            		meanNormalVector_R = mean(normalVectors_R, 1);
-            		meanNormalVector_R = VecNormalize(meanNormalVector_R);
-            
-            		vecOrthogonal_R = dot(reconstructedVec_R, meanNormalVector_R) * meanNormalVector_R;
-            		modVec_R = reconstructedVec_R - vecOrthogonal_R;
-            		modVec_R = VecNormalize(modVec_R);
-
-            		% Store the flattened vectors for later use or visualization
-            		flattenedVectors_L(F, :) = modVec_L;
-            		flattenedVectors_R(F, :) = modVec_R;			
 
 			% remove orthogonal component of vectors to flatten to inflated surface
 			% find the faces involved in this vertex 
@@ -249,21 +205,26 @@ for prop=1:3
 		end
 		% end for each point in space
 		% plot it!
-		figure('units','pixels','position',[0 0 2000 2000])
-		trisurf(faces_li, vx_li(:, 1), vx_li(:, 2), vx_li(:, 3),signalOfInt_L , 'EdgeColor','none')
-		hold on;
-		quiver3d(L_inc(1:1345,1),L_inc(1:1345,2),L_inc(1:1345,3),flattenedVectors_L(1:1345,1),flattenedVectors_L(1:1345,2),flattenedVectors_L(1:1345,3),[0.2 0.2 0.8])
-		view([90 0]);
-		colormap(roybigbl_cm);
-		daspect([1 1 1]);
-		axis tight;
-		axis vis3d off;
-		lighting none;
-		shading flat;
-		camlight;
-		colorbar
-		fn=['~/boldvec_' subj '_' sesh '_Prop' num2str(prop) '_fr' num2str(fr) '.png'];
-        	print(fn,'-dpng')
+
+		fn=['~/boldvec_' subj '_' sesh '_Prop' num2str(prop) '_fr' num2str(fr) '_directions.png'];
+		Vis_Surf_n_Vecfield(signalOfInt_L,signalOfInt_R,flattenedVectors_L,flattenedVectors_R,fn,'Directional');
+		fn=['~/boldvec_' subj '_' sesh '_Prop' num2str(prop) '_fr' num2str(fr) '_BOLD.png'];
+                Vis_Surf_n_Vecfield(signalOfInt_L,signalOfInt_R,flattenedVectors_L,flattenedVectors_R,fn,'BOLD');
+		%figure('units','pixels','position',[0 0 2000 2000])
+		%trisurf(faces_li, vx_li(:, 1), vx_li(:, 2), vx_li(:, 3),signalOfInt_L , 'EdgeColor','none')
+		%hold on;
+		%quiver3d(L_inc(1:1345,1),L_inc(1:1345,2),L_inc(1:1345,3),flattenedVectors_L(1:1345,1),flattenedVectors_L(1:1345,2),flattenedVectors_L(1:1345,3),[0.2 0.2 0.8])
+		%view([90 0]);
+		%colormap(roybigbl_cm);
+		%daspect([1 1 1]);
+		%axis tight;
+		%axis vis3d off;
+		%lighting none;
+		%shading flat;
+		%camlight;
+		%colorbar
+        	%print(fn,'-dpng')
+		
 		% print it out: 4 views (lateral + medial *L/R)
 	% end for this frame
 	end
--- scripts/Vis_Surf_n_Vecfield.m
+++ scripts/Vis_Surf_n_Vecfield.m
@@ -1,7 +1,8 @@
-function Vis_Surf_n_Vecfield(surfl,surfr,vecl,vecr,Fn) 
+function Vis_Surf_n_Vecfield(surfl,surfr,vecl,vecr,Fn,Coloration) 
 addpath(genpath('/oak/stanford/groups/leanew1/users/apines/libs'))
 % designed for fsaverage4 surface
 VertVecL=surfl;
+VertVecR=surfr;
 %%% Load in surface data
 SubjectsFolder = '/oak/stanford/groups/leanew1/users/apines/surf';
 surfL = [SubjectsFolder '/lh.pial'];
@@ -29,90 +30,89 @@ ret=vecl;
 scalingfactor=1;
 % colors
 mincol=0;
-maxcol=max(vecl(:));
-% scale RGB values to max
-minVal = min(vecl(:));
-maxVal = max(vecl(:));
-% consider scaling RGB colors by this value as well
-mincol=0;
-maxcol=max(vecl(:));
-% scale RGB values to max
-minVal = min(vecl(:));
-maxVal = max(vecl(:));
-% need everything to be above 0 (+absminval) but scaled 0-1 (./maxval+absminval)
-RGBValues=(vecl+abs(minVal))./(maxVal+abs(minVal));
-
-%%% for red/blue 0-centered
-%mincol=-9;
-%maxcol=9;
-%custommap=colormap(b2r(mincol,maxcol));
-% abscense of color to gray to accom. lighting "none"
-%custommap(126,:)=[.5 .5 .5];
-%custommap=colormap(jet)
-
-%%% matches circular hist
-% for 180 degree max
-%roybigbl_cm=inferno(6);
-%roybigbl_cm(1,:)=[0, 0, 255];
-%roybigbl_cm(2,:)=[0, 255, 255];
-%roybigbl_cm(3,:)=[116, 192, 68];
-%roybigbl_cm(4,:)=[246, 235, 20];
-%roybigbl_cm(5,:)=[255, 165, 0];
-%roybigbl_cm(6,:)=[255, 0, 0];
-% scale to 1
-%roybigbl_cm=roybigbl_cm.*(1/255);
-% interpolate color gradient
-%interpsteps=[0 .2 .4 .6 .8 1];
-%roybigbl_cm=interp1(interpsteps,roybigbl_cm,linspace(0,1,255));
-% make circular with flipud
-%custommap=vertcat(flipud(roybigbl_cm),roybigbl_cm);
-
-% flatten vectors to surface
-% medial left hemisphere
-[vertices, faces] = freesurfer_read_surf([SubjectsFolder '/lh.sphere']);
-% make VertVecL parallel to pial surface as "ret"
-% for each vector, subtract weighted surface-orthogonal component from original vector
-for v=1:length(vertices)
-        % retrieve original vector
-        OGvec=ret(v,:);
-        % find the three faces involved in this vertex 
-        [InvolvedFaces,~]=find(faces==v);
-        % get normal vectors of each involved face
-        normalVectors = cross(vertices(faces(InvolvedFaces, 2), :) - vertices(faces(InvolvedFaces, 1), :), vertices(faces(InvolvedFaces, 3), :) - vertices(faces(InvolvedFaces, 1), :));
-        % find vector as close as possible to orthogonal from these three faces
-        meanNormalVector = mean(normalVectors, 1);
-        % normalize normal vector
-        meanNormalVector=VecNormalize(meanNormalVector);
-        % get dot product of orthogonal vector and original vector
-        OGvecOrthogonal = dot(OGvec, meanNormalVector) * meanNormalVector;
-        % subtract orthogonal component of original vector from original vector
-        modVec = OGvec - OGvecOrthogonal;;
-        % add modified vector to initialized matrix
-        ret(v,:)=VecNormalize(modVec);
+if strcmp(Coloration,'Directional')
+	maxcol=max(vecl(:));
+	% scale RGB values to max
+	minVal = min(vecl(:));
+	maxVal = max(vecl(:));
+	% consider scaling RGB colors by this value as well
+	mincol=0;
+	maxcol=max(vecl(:));
+	% scale RGB values to max
+	minVal = min(vecl(:));
+	maxVal = max(vecl(:));
+	% need everything to be above 0 (+absminval) but scaled 0-1 (./maxval+absminval)
+	RGBValues=(vecl+abs(minVal))./(maxVal+abs(minVal));
+elseif strcmp(Coloration,'BOLD')
+	%mincol=min(min([VertVecL VertVecR]))
+	%maxcol=max(max([VertVecL VertVecR]))
+	mincol=-10;
+	maxcol=10;
 end
+
 % replace nans with 0
 ret(isnan(ret))=0;
-% set Rgb color map
-custommap = [
-    1 0 0;    % Red
-    0 1 0;    % Green
-    0 0 1;    % Blue
-    1 1 0;    % Yellow
-    0 1 1;    % Cyan
-    1 0 1;    % Magenta
-    0 0 0;    % Black
-    1 1 1     % White
-];
-% 0 to gray
-%custommap=colormap('jet');
+if strcmp(Coloration,'Directional')
+	% if colormap=directional, Rgb color map
+	custommap = [
+	    1 0 0;    % Red
+	    0 1 0;    % Green
+	    0 0 1;    % Blue
+	    1 1 0;    % Yellow
+	    0 1 1;    % Cyan
+	    1 0 1;    % Magenta
+	    0 0 0;    % Black
+	    1 1 1     % White
+	];
+elseif strcmp(Coloration,'BOLD')
+	roybigbl_cm=inferno(16);
+	roybigbl_cm(1,:)=[255, 255, 0 ];
+	roybigbl_cm(2,:)=[255, 200, 0];
+	roybigbl_cm(3,:)=[255, 120, 0];
+	roybigbl_cm(4,:)=[255, 0, 0 ];
+	roybigbl_cm(5,:)=[200, 0, 0 ];
+	roybigbl_cm(6,:)=[150, 0, 0 ];
+	roybigbl_cm(7,:)=[100, 0, 0 ];
+	roybigbl_cm(8,:)=[60, 0, 0 ];
+	roybigbl_cm(9,:)=[0, 0, 80 ];
+	roybigbl_cm(10,:)=[0, 0, 170];
+	roybigbl_cm(11,:)=[75, 0, 125];
+	roybigbl_cm(12,:)=[125, 0, 160];
+	roybigbl_cm(13,:)=[75, 125, 0];
+	roybigbl_cm(14,:)=[0, 200, 0];
+	roybigbl_cm(15,:)=[0, 255, 0];
+	roybigbl_cm(16,:)=[0, 255, 255];
+	% scale to 1
+	roybigbl_cm=roybigbl_cm.*(1/255);
+	% interpolate color gradient
+	interpsteps=[0 0.0666 0.1333 0.2 0.2666 0.3333 0.4 0.4666 0.5333 0.6 0.6666 0.7333 0.8 0.86666 0.9333 1];
+	roybigbl_cm=interp1(interpsteps,roybigbl_cm,linspace(0,1,255));
+	% yellow as high
+	roybigbl_cm=flipud(roybigbl_cm);
+	% reduce just a little bit on the close-to-white coloring
+	custommap=roybigbl_cm(15:240,:);
+end
 % medial left hemisphere
 [vertices, faces] = freesurfer_read_surf([SubjectsFolder '/lh.inflated']);
 % begin figure
 figure
 asub = subaxis(2,2,1, 'sh', 0, 'sv', 0, 'padding', 0, 'margin', 0);
-aplot = trisurf(faces, vertices(:,1), vertices(:,2), vertices(:,3))
+if strcmp(Coloration,'Directional')
+	aplot = trisurf(faces, vertices(:,1), vertices(:,2), vertices(:,3))
+	set(aplot, 'EdgeColor', 'none');
+	set(aplot, 'FaceColor', 'w');
+	set(aplot, 'LineStyle', 'none');
+elseif strcmp(Coloration,'BOLD')
+	aplot = trisurf(faces, vertices(:,1), vertices(:,2), vertices(:,3))
+	aplot.FaceVertexCData=data;
+end
 hold on;
-quiver3D(vertices(:,1),vertices(:,2),vertices(:,3),ret(:,1), ret(:,2), ret(:,3),RGBValues,scalingfactor)
+% create a quiver if this is a directional plot, fill-in if not
+if strcmp(Coloration,'Directional')
+	quiver3D(vertices(:,1),vertices(:,2),vertices(:,3),ret(:,1), ret(:,2), ret(:,3),RGBValues,scalingfactor)
+elseif strcmp(Coloration,'BOLD')
+	quiver3D(0,0,0,0, 0, 0,0,scalingfactor)
+end
 view([90 0]);
 colormap(custommap)
 daspect([1 1 1]);
@@ -120,21 +120,28 @@ axis tight;
 axis vis3d off;
 lighting none;
 shading flat;
-camlight;
-
 set(gca,'CLim',[mincol,maxcol]);
-aplot.FaceVertexCData=data;
-aplot.FaceAlpha=.3;
-
 % other view of left hemisphere (lateral)
 asub = subaxis(2,2,4, 'sh', 0.00, 'sv', 0.00, 'padding', 0, 'margin', 0);
-aplot = trisurf(faces, vertices(:,1), vertices(:,2), vertices(:,3))
+if strcmp(Coloration,'Directional')
+        aplot = trisurf(faces, vertices(:,1), vertices(:,2), vertices(:,3))
+	set(aplot, 'EdgeColor', 'none');
+	set(aplot, 'FaceColor', 'w');
+	set(aplot, 'LineStyle', 'none');
+elseif strcmp(Coloration,'BOLD')
+        aplot = trisurf(faces, vertices(:,1), vertices(:,2), vertices(:,3))
+        aplot.FaceVertexCData=data;
+end
 view([90 0]);
 hold on;
 colormap(custommap);
 caxis([mincol; maxcol]);
 % removing upscaling vector field so vectors are locked to vertices
-bplot=quiver3D(vertices(:,1),vertices(:,2),vertices(:,3),ret(:,1), ret(:,2), ret(:,3),RGBValues,scalingfactor)
+if strcmp(Coloration,'Directional')
+	bplot=quiver3D(vertices(:,1),vertices(:,2),vertices(:,3),ret(:,1), ret(:,2), ret(:,3),RGBValues,scalingfactor)
+elseif strcmp(Coloration,'BOLD')
+        bplot=quiver3D(0,0,0,0, 0, 0,0,scalingfactor)
+end
 daspect([1 1 1]);
 axis tight;
 rotate(aplot, [0 0 1], 180)
@@ -142,15 +149,9 @@ rotate(bplot, [0 0 1], 180)
 
 axis vis3d off;
 lighting none;
-material metal %shiny %metal;
 shading flat;
-camlight;
-
-% insert RGB colors onto surface
-aplot.FaceVertexCData=data;
-aplot.FaceAlpha=.3;
 % printout
-print(Fn,'-dpng','-r2000')
+print(Fn,'-dpng','-r1000')
 
 
 
