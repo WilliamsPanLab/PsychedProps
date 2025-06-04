0d60c401097221900ee344cea409729e93ce5ab4
adpines <apines@stanford.edu>
2025-06-04 08:41:26 -0700

    lsd init

---
+++ scripts/BP_cifti.m
@@ -0,0 +1,42 @@
+function BP_cifti(subj,sesh,task)
+% addpaths
+ToolFolder='/oak/stanford/groups/leanew1/users/apines/scripts/PersonalCircuits/scripts/code_nmf_cifti/tool_folder';
+addpath(genpath(ToolFolder));
+
+% Load CIFTI
+parentfp=['/scratch/users/apines/LSD_ICL/rest_proc/' subj '/' sesh '/']; 
+infile = [parentfp sesh '/' subj '_' sesh '_' task '.dtseries.nii'];
+outname = [[parentfp sesh '/' subj '_' sesh '_' task '_filt.dtseries.nii'];
+TR = 2;
+Fs = 1 / TR;
+N = 2;            % 2nd-order Butterworth
+
+% filter range matched
+low_cutoff = 0.009;
+high_cutoff = 0.08;
+
+% load in
+ts = read_cifti(infile);
+data = ts.dtseries;
+n_time = size(data, 2);
+
+% crete filter
+[b, a] = butter(N, [low_cutoff high_cutoff] / (Fs / 2), 'bandpass');
+
+% Filter first 5000 grayordinates
+data_filt = data;   % copy to preserve structure
+for g = 1:size(data,1)
+    signal = data(g, :);
+    if any(isnan(signal)) || all(signal==0)
+        continue  % skip bad rows
+    end
+    signal_filt = filtfilt(b, a, double(signal));
+    data_filt(g, :) = signal_filt;
+end
+
+% Replace dtseries with filtered version
+ts.dtseries = data_filt;
+
+% Save to new CIFTI file
+write_cifti(outname, ts, 'parameter', 'dtseries');
+
---
+++ scripts/sbatch_OpFl_lsd.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+#
+#SBATCH --job-name=OpFl
+#SBATCH --time=40:00:00 # need >1 (try 10-12 to be safe) if running opfl
+#SBATCH -n 4 # try 4 if running opfl
+#SBATCH --mem=15G # up to 25 if running opfl
+#SBATCH -p normal,leanew1  # Queue names you can submit to
+# Outputs ----------------------------------
+#SBATCH --mail-user=apines@stanford.edu
+#SBATCH --mail-type=ALL
+# ------------------------------------------
+
+
+############    Buffaflow   ###################
+##             _.-````'-,,
+##   _,.,_ ,-'`           ``''-.,
+## /)     (\                   '``-.
+##((      ) )                ->    `\
+## \)    (_/               ->       )\
+##  |       /)           '    ,'    / \
+##  `\    ^'            '     (    /  ))
+##    |      _/\ ,     /    ,,`\   (  "`
+##     \Y,   |  \  \  /____| / \_ \##
+# This script is to prep the environment of a slurm node, 
+##       `)_/    \  \  )    (->  (->
+# Launch optical flow, caclulate angular distances, generate figures
+##                \( \(     |/   |/
+# and delete interim files upon completion
+##    mic & dwb  /_(/_(    /_(  /_(
+
+
+############################
+#### module I: Preprocessing
+############################
+echo "ΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔ"
+echo "Starting module I: preproc"
+echo "ΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔ"
+# will need matlab
+module load matlab
+# and freesurfer
+module load biology
+module load freesurfer/7.3.2
+# and workbench
+module load workbench
+module load contribs poldrack anaconda/5.0.0-py36
+# subject name is input argument
+subj=$1
+# sesh is input 2
+sesh=$2
+
+
+# mask resting-state out from aggregate cifti
+##matlab -nodisplay -r "RS_mask_lsd('$subj','$sesh')"
+
+# BP data
+matlab -nodisplay -r "BP_cifti('$subject','$sesh','rs1')"
+matlab -nodisplay -r "BP_cifti('$subject','$sesh','rs2')"
+matlab -nodisplay -r "BP_cifti('$subject','$sesh','mus')"
+
+# Downsample the data 
+##/oak/stanford/groups/leanew1/users/apines/scripts/OpFl_CDys/scripts/DS_surf_ts_lsd.sh $1 $2
+#sleep 20
+
+# cd to workaround addpath in matlab shell call
+cd /oak/stanford/groups/leanew1/users/apines/scripts/OpFl_CDys/scripts
+
+# mask images: 6+ continuous frames only
+##matlab -nodisplay -r "MotMask_lsd('$subj','$sesh','rs1')"
+##matlab -nodisplay -r "MotMask_lsd('$subj','$sesh','rs2')"
+#matlab -nodisplay -r "MotMask_lsd('$subj','$sesh','mus')"
+
+############################
+#### module II: Optical Flow
+############################
+echo "ΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔ"
+echo "Starting module II: Optical Flow"
+echo "ΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔ"
+# Calculate Optical Flow
+#matlab -nodisplay -r "OpFl_lsd('$subj','$sesh','rs1')"
+#matlab -nodisplay -r "OpFl_lsd('$subj','$sesh','rs2')"
+#matlab -nodisplay -r "OpFl_lsd('$subj','$sesh','mus')"
+
+
+#############################
+#### module III: Calc. Angles
+#############################
+echo "ΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔ"
+echo "Starting module III: Angular distance calculation"
+echo "ΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔΔ"
+
+# extract relative angles
+#matlab -nodisplay -r "Extract_RelativeAngles_lsd('$subj','$sesh','rs1')"
+#matlab -nodisplay -r "Extract_RelativeAngles_lsd('$subj','$sesh','rs2')"
+#matlab -nodisplay -r "Extract_RelativeAngles_lsd('$subj','$sesh','mus')"
+
+# and magnitudes
+#matlab -nodisplay -r "Extract_DMNMag_lsd('$subj','$sesh','rs1')"
+#matlab -nodisplay -r "Extract_DMNMag_lsd('$subj','$sesh','rs2')"
+#matlab -nodisplay -r "Extract_DMNMag_lsd('$subj','$sesh','mus')"
+
+# DMN seg
+#matlab -nodisplay -r "Extract_DMNSeg_lsd('$subj','$sesh','rs1')"
+#matlab -nodisplay -r "Extract_DMNSeg_lsd('$subj','$sesh','rs2')"
+#matlab -nodisplay -r "Extract_DMNSeg_lsd('$subj','$sesh','mus')"
+#
+
+# autocor
+#matlab -nodisplay -r "Extract_TAutoCor_lsd('$subj','$sesh','rs1')"
+#matlab -nodisplay -r "Extract_TAutoCor_lsd('$subj','$sesh','rs2')"
+#matlab -nodisplay -r "Extract_TAutoCor_lsd('$subj','$sesh','mus')"
+
+#############################
+#### module IV: Calc Spun Metrics
+#############################
+
+# extract magnitudes
+matlab -nodisplay -r "Extract_DMNMag_Spun_lsd('$subj','$sesh','rs1')"
+matlab -nodisplay -r "Extract_DMNMag_Spun_lsd('$subj','$sesh','rs2')"
+matlab -nodisplay -r "Extract_DMNMag_Spun_lsd('$subj','$sesh','mus')"
+
+echo spunMags done
+
+# extract relative angles
+matlab -nodisplay -r "Extract_RelativeAngles_Spun_lsd('$subj','$sesh','rs1')"
+matlab -nodisplay -r "Extract_RelativeAngles_Spun_lsd('$subj','$sesh','rs2')"
+matlab -nodisplay -r "Extract_RelativeAngles_Spun_lsd('$subj','$sesh','mus')"
+
+echo spunAngles done
+
+# extract DMN FC
+matlab -nodisplay -r "Extract_DMNSeg_Spun_lsd('$subj','$sesh','rs1')"
+matlab -nodisplay -r "Extract_DMNSeg_Spun_lsd('$subj','$sesh','rs2')"
+matlab -nodisplay -r "Extract_DMNSeg_Spun_lsd('$subj','$sesh','mus')"
+
+echo spunFC done
+
+# extract AutoCor
+matlab -nodisplay -r "Extract_TAutoCor_Spun_lsd('$subj','$sesh','rs1')"
+matlab -nodisplay -r "Extract_TAutoCor_Spun_lsd('$subj','$sesh','rs2')"
+matlab -nodisplay -r "Extract_TAutoCor_Spun_lsd('$subj','$sesh','mus')"
+
+echo spunAutocor done
+#################
+echo "OpFl complete"
